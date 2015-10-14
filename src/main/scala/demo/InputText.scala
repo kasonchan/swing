c655df3a3453a3ccf0a76e4885a917c9bad1d636
KasonChan <kasonl.chan@gmail.com>
2015-10-13 20:12:12 -0500

    Add InputText

--- src/main/scala/demo/InputText.scala
+++ src/main/scala/demo/InputText.scala
@@ -1,8 +1,29 @@
 package demo
 
+import scala.swing.BorderPanel.Position._
+import scala.swing.event.{Key, KeyPressed}
+import scala.swing.{BorderPanel, Label, MainFrame, SimpleSwingApplication, TextField}
+
 /**
  * Created by kasonchan on 10/13/15.
  */
-object InputText {
+object InputText extends SimpleSwingApplication {
+
+  def top = new MainFrame {
+    title = "TextFieldSample"
+    val textfield: TextField = new TextField("Input")
+    val label: Label = new Label("Output")
+
+    contents = new BorderPanel() {
+      add(textfield, North)
+      add(label, South)
+    }
+
+    listenTo(textfield.keys)
+
+    reactions += {
+      case KeyPressed(_, Key.Enter, _, _) => label.text_=(textfield.text)
+    }
+  }
 
 }
