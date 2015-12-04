a85a872254404a9d8fe481984e43b5eb27e688bf
KasonChan <kasonl.chan@gmail.com>
2015-12-04 16:08:56 -0600

    Add icon.png, read.txt, set full screen, get system property, and read lines from resources file.

--- src/main/scala/demo/Button.scala
+++ src/main/scala/demo/Button.scala
@@ -1,5 +1,8 @@
 package demo
 
+import java.awt.Toolkit
+
+import scala.io.Source
 import scala.swing._
 import scala.swing.event.ButtonClicked
 
@@ -10,17 +13,24 @@ object Button extends SimpleSwingApplication {
 
   def top = new MainFrame {
     title = "Button"
+
+    // Set the app to full screen
+    val screenSize = Toolkit.getDefaultToolkit.getScreenSize
+    preferredSize = screenSize
+
     contents = new GridPanel(2, 2) {
       hGap = 3
       vGap = 3
       contents += new Button {
         text = "Press Me!"
         reactions += {
-          case ButtonClicked(_) => text = "Hello Scala"
+          case ButtonClicked(_) =>
+            val os = System.getProperty("os.name")
+            val read = Source.fromURL(getClass.getResource("/read.txt")).getLines().mkString
+            text = os + ". Hello everyone! " + read
         }
       }
     }
-    size = new Dimension(300, 300)
   }
 
 }
--- src/main/scala/demo/InputText.scala
+++ src/main/scala/demo/InputText.scala
@@ -1,8 +1,12 @@
 package demo
 
+import java.awt.Toolkit
+import javax.swing.ImageIcon
+
+import scala.io.Source
 import scala.swing.BorderPanel.Position._
+import scala.swing._
 import scala.swing.event.{Key, KeyPressed}
-import scala.swing.{BorderPanel, Label, MainFrame, SimpleSwingApplication, TextField}
 
 /**
  * Created by kasonchan on 10/13/15.
@@ -11,8 +15,28 @@ object InputText extends SimpleSwingApplication {
 
   def top = new MainFrame {
     title = "TextFieldSample"
-    val textfield: TextField = new TextField("Input")
-    val label: Label = new Label("Output")
+
+    // Set the app to full screen
+    val screenSize = Toolkit.getDefaultToolkit.getScreenSize
+    preferredSize = screenSize
+
+    val os = System.getProperty("os.name")
+
+    os match {
+      case "Mac OS X" =>
+        //       Set launch icon in Mac OS
+        com.apple.eawt.Application.getApplication
+          .setDockIconImage(new ImageIcon(getClass
+            .getResource("/icon.png")).getImage)
+      case _ =>
+        Toolkit.getDefaultToolkit.getImage(getClass.getResource("/icon.png"))
+    }
+    
+    val textfield: TextField = new TextField()
+
+    // Read lines from resources file
+    val read = Source.fromURL(getClass.getResource("/read.txt")).getLines().mkString
+    val label: Label = new Label(os + ". " + read)
 
     contents = new BorderPanel() {
       add(textfield, North)
@@ -22,7 +46,11 @@ object InputText extends SimpleSwingApplication {
     listenTo(textfield.keys)
 
     reactions += {
-      case KeyPressed(_, Key.Enter, _, _) => label.text_=(textfield.text)
+      case KeyPressed(_, Key.Enter, _, _) =>
+        textfield.text.isEmpty match {
+          case false => label.text_=(textfield.text)
+          case true => label.text_=("None")
+        }
     }
   }
 
